0383c02c5ae5d7d16927a81eba9d90b24b4d6738
MingsonZheng <mingsonzheng@outlook.com>
2023-11-06 15:23:52 +0800

    整型

---
+++ chapter002/Integer.java
@@ -0,0 +1,10 @@
+//整型
+public class Integer {
+    public static void main(String[] args) {
+        
+        //Java的整型常量（具体值）默认为int型，声明long型常量须后加‘l’或者‘L’
+        int n1 = 1;//4个字节
+        // int n2 = 1L;//不对
+        long n3 = 1L;//对
+    }
+}
