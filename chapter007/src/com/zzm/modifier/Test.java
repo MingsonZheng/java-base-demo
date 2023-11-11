c802d43e984533b4778c8dac43460270227c7728
MingsonZheng <mingsonzheng@outlook.com>
2023-11-11 20:32:32 +0800

    访问修饰符

---
+++ chapter007/src/com/zzm/modifier/Test.java
@@ -0,0 +1,16 @@
+// // 访问修饰符
+package com.zzm.modifier;
+
+public class Test {
+    public static void main(String[] args) {
+        A a = new A();
+        a.m1();
+        B b = new B();
+        b.say();
+    }
+}
+
+// 只有 默认和public 可以修饰类
+class Tiger {
+
+}
