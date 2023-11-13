bec036ff370d3839117e4777bdb15f445d0a98fe
MingsonZheng <mingsonzheng@outlook.com>
2023-11-13 22:04:04 +0800

    多态数组

---
+++ chapter007/src/com/zzm/poly_/polyarr_/Student.java
@@ -0,0 +1,29 @@
+// 多态数组
+package com.zzm.poly_.polyarr_;
+
+public class Student extends Person {
+    private double score;
+
+    public Student(String name, int age, double score) {
+        super(name, age);
+        this.score = score;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    @Override
+    public String say() {
+        return super.say() + " score=" + score;
+    }
+
+    // 特有的方法
+    public void study() {
+        System.out.println("学生 " + getName() + " 正在学java");
+    }
+}
