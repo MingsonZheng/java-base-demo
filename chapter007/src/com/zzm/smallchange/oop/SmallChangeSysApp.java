a3281f0f331f90f50c5e77b220a80f8536f32772
MingsonZheng <mingsonzheng@outlook.com>
2023-11-14 16:59:20 +0800

    零钱通项目

---
+++ chapter007/src/com/zzm/smallchange/oop/SmallChangeSysApp.java
@@ -0,0 +1,11 @@
+// 零钱通项目OOP版
+package com.zzm.smallchange.oop;
+
+/**
+ * 这里我们直接调用SmalLChangeSys0OP 对象，显示主英单即可
+ */
+public class SmallChangeSysApp {
+    public static void main(String[] args) {
+        new SmallChangeSysOOP().mainMenu();
+    }
+}
