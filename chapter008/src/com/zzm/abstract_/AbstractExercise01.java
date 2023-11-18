7e3c4f265b2c4214b2638126633bb6c4e2075bd1
MingsonZheng <mingsonzheng@outlook.com>
2023-11-18 16:14:11 +0800

    抽象类

---
+++ chapter008/src/com/zzm/abstract_/AbstractExercise01.java
@@ -0,0 +1,13 @@
+// 抽象类练习
+package com.zzm.abstract_;
+
+public class AbstractExercise01 {
+    public static void main(String[] args) {
+        Manager jack = new Manager("jack", 999, 50000);
+        jack.setBonus(8000);
+        jack.work();
+
+        CommonEmployee tom = new CommonEmployee("tom", 888, 20000);
+        tom.work();
+    }
+}
