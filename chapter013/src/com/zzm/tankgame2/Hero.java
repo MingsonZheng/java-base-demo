75db510b43acbd6a6d63ba98704b78d22e34a9bb
MingsonZheng <mingsonzheng@outlook.com>
2023-12-01 00:21:37 +0800

    坦克大战

---
+++ chapter013/src/com/zzm/tankgame2/Hero.java
@@ -0,0 +1,12 @@
+package com.zzm.tankgame2;
+
+/**
+ * @author Mingson
+ * @version 1.0
+ * 自己的坦克
+ */
+public class Hero extends Tank {
+    public Hero(int x, int y) {
+        super(x, y);
+    }
+}
--- chapter013/src/com/zzm/tankgame2/ZzmTankGame02.java
+++ chapter013/src/com/zzm/tankgame2/ZzmTankGame02.java
@@ -1,24 +1,25 @@
-package com.zzm.tankgame;
+package com.zzm.tankgame2;
 
 import javax.swing.*;
 
 /**
  * @author Mingson
  * @version 1.0
- * 绘制出了我方的坦克
+ * 通过按键可以控制我方坦克上下左右移动
  */
-public class ZzmTankGame01 extends JFrame {
+public class ZzmTankGame02 extends JFrame {
 
     // 定义 MyPanel
     MyPanel mp = null;
     public static void main(String[] args) {
-        ZzmTankGame01 zzmTankGame01 = new ZzmTankGame01();
+        ZzmTankGame02 zzmTankGame02 = new ZzmTankGame02();
     }
 
-    public ZzmTankGame01() {
+    public ZzmTankGame02() {
         mp = new MyPanel();
         this.add(mp);// 面板(就是游戏的绘图区域)
         this.setSize(1000, 750);
+        this.addKeyListener(mp);// 让JFrame 监听mp的键盘事件
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
     }
