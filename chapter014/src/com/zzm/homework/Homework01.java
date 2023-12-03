177b4927b7e4a4446f0fdf72978e2f9f0a9f404c
MingsonZheng <mingsonzheng@outlook.com>
2023-12-03 18:09:48 +0800

    作业

---
+++ chapter014/src/com/zzm/homework/Homework01.java
@@ -0,0 +1,69 @@
+package com.zzm.homework;
+
+import java.util.Scanner;
+
+/**
+ * @author Mingson
+ * @version 1.0
+ */
+public class Homework01 {
+    public static void main(String[] args) {
+        A a = new A();
+        B b = new B(a);
+        a.start();
+        b.start();
+    }
+}
+
+// 创建A线程类
+class A extends Thread {
+    private boolean loop = true;
+
+    public boolean isLoop() {
+        return loop;
+    }
+
+    public void setLoop(boolean loop) {
+        this.loop = loop;
+    }
+
+    @Override
+    public void run() {
+        // 输出1-100数字
+        while (loop) {
+            System.out.println((int)(Math.random() * 100 + 1));
+            // 休眠
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("a线程退出.");
+    }
+}
+
+// 直到第2个线程从键盘读取了“Q"命令
+class B extends Thread {
+    private A a;
+    private Scanner scanner = new Scanner(System.in);
+
+    public B(A a) {
+        this.a = a;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            // 接收到用户的输入
+            System.out.println("请输入你指令");
+            char key = scanner.next().toUpperCase().charAt(0);
+            if (key == 'Q') {
+                // 以通知的方式结束A线程
+                a.setLoop(false);
+                System.out.println("b线程退出.");
+                break;
+            }
+        }
+    }
+}
