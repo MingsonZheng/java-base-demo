2940b2b7e265fd67650b68f02999401b35ba011a
MingsonZheng <mingsonzheng@outlook.com>
2023-12-06 16:11:44 +0800

    ObjectOutputStream

---
+++ chapter016/src/com/zzm/outputstream_/Dog.java
@@ -0,0 +1,43 @@
+// ObjectOutputStream
+package com.zzm.outputstream_;
+
+import java.io.Serializable;
+
+/**
+ * @author Mingson
+ * @version 1.0
+ */
+// 如果需要序列化某个类的对象，实现 Serializable
+public class Dog implements Serializable {
+    private String name;
+    private int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- chapter016/src/com/zzm/outputstream_/ObjectOutputStream_.java
+++ chapter016/src/com/zzm/outputstream_/ObjectOutputStream_.java
@@ -3,7 +3,6 @@ package com.zzm.outputstream_;
 
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 
 /**
  * @author Mingson
@@ -28,16 +27,5 @@ public class ObjectOutputStream_ {
 
         oos.close();
         System.out.println("数据保存完毕(序列化形式)");
-    }
-}
-
-// 如果需要序列化某个类的对象，实现 Serializable
-class Dog implements Serializable {
-    private String name;
-    private int age;
-
-    public Dog(String name, int age) {
-        this.name = name;
-        this.age = age;
     }
 }
