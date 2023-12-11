e43029b8c82f81db3aa3b7de11e424d1421b290e
MingsonZheng <mingsonzheng@outlook.com>
2023-12-11 20:30:47 +0800

    Class常用方法

---
+++ chapter019/src/com/zzm/Car.java
@@ -0,0 +1,21 @@
+// Class常用方法
+package com.zzm;
+
+/**
+ * @author Mingson
+ * @version 1.0
+ */
+public class Car {
+    public String brand = "宝马";
+    public int price = 500000;
+    public String color = "白色";
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
