e7b70df10cd9b16fc4aefecbf544daa906d9ce91
MingsonZheng <mingsonzheng@outlook.com>
2023-12-11 19:10:25 +0800

    反射相关类

--- chapter019/src/com/zzm/Cat.java
+++ chapter019/src/com/zzm/Cat.java
@@ -1,4 +1,4 @@
-// 反射快速入门
+// 反射快速入门，反射相关类
 package com.zzm;
 
 /**
@@ -8,6 +8,14 @@ package com.zzm;
 public class Cat {
 
     private String name = "招财猫";
+    public int age = 0;
+
+    public Cat() {
+    }
+
+    public Cat(String name) {
+        this.name = name;
+    }
 
     public void hi() {// 常用方法
         System.out.println("hi " + name);
