e2260b4cdd99fde8c2dee2bd0c50c4144da42e6a
MingsonZheng <mingsonzheng@outlook.com>
2023-12-18 17:43:03 +0800

    JDBCUtilsDML和查询

--- chapter020/src/com/zzm/jdbc/utils/JDBCUtils_Use.java
+++ chapter020/src/com/zzm/jdbc/utils/JDBCUtils_Use.java
@@ -1,11 +1,9 @@
-// JDBCUtilsDML
+// JDBCUtilsDML和查询
 package com.zzm.jdbc.utils;
 
 import org.junit.jupiter.api.Test;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 /**
  * @author Mingson
@@ -13,6 +11,39 @@ import java.sql.SQLException;
  * 该类演示如何使用JDBCUtils 工具类，完成dml 和 select
  */
 public class JDBCUtils_Use {
+
+    @Test
+    public void testSelect() {
+        // 1. 得到连接
+        Connection connection = null;
+        // 2. 组织一个sql
+        // String sql = "select * from actor";
+        String sql = "select * from actor where id = ?";
+        PreparedStatement preparedStatement = null;
+        ResultSet set = null;
+        // 3. 创建PreparedStatement 对象
+        try {
+            connection = JDBCUtils.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, 2);// 给?号赋值
+            // 执行，得到结果集
+            set = preparedStatement.executeQuery();
+            // 遍历该结果集
+            while (set.next()) {
+                int id = set.getInt("id");
+                String name = set.getString("name");
+                String sex = set.getString("sex");
+                Date borndate = set.getDate("borndate");
+                String phone = set.getString("phone");
+                System.out.println(id + "\t" + name + "\t" + sex + "\t" + borndate + "\t" + phone);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            JDBCUtils.close(set, preparedStatement, connection);
+        }
+    }
 
     @Test
     public void testDML() {//insert, update, delete
