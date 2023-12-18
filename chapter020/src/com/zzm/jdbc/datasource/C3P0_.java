8be5a98792c8c0b36ae35b4de71ab249becbea4c
MingsonZheng <mingsonzheng@outlook.com>
2023-12-18 23:57:31 +0800

    C3P0的使用

---
+++ chapter020/src/com/zzm/jdbc/datasource/C3P0_.java
@@ -0,0 +1,55 @@
+// C3P0的使用
+package com.zzm.jdbc.datasource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.util.Properties;
+
+/**
+ * @author Mingson
+ * @version 1.0
+ * 演示c3p0的使用
+ */
+public class C3P0_ {
+    // 前置工作: 在项目下创建一个文件夹比如 libs
+    // 将 c3p0.jar, mchange.jar 拷贝到该目录下，点击 add to project ..加入到项目中
+
+    // 方式1: 相关参数，在程序中指定user，url，password等
+    @Test
+    public void testC3P0_01() throws Exception {
+        // 1. 创建一个数据源对象
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        // 2. 通过配置文件mysql.properties 获取相关连接的信息
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("src\\mysql.properties"));
+        // 读取相关的属性值
+        String user = properties.getProperty("user");
+        String password = properties.getProperty("password");
+        String url = properties.getProperty("url");
+        String driver = properties.getProperty("driver");
+
+        // 给数据源 comboPooledDataSource 设置相关的参数
+        // 注意:连接管理是由 comboPooledDataSource 来管理
+        comboPooledDataSource.setDriverClass(driver);
+        comboPooledDataSource.setJdbcUrl(url);
+        comboPooledDataSource.setUser(user);
+        comboPooledDataSource.setPassword(password);
+
+        // 设置初始化连接数
+        comboPooledDataSource.setInitialPoolSize(10);
+        // 最大连接数
+        comboPooledDataSource.setMaxPoolSize(50);
+        // 测试连接池的效率，测试对mysql 5000次操作
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 5000; i++) {
+            Connection connection = comboPooledDataSource.getConnection();// 这个方法就是从 DataSource 接口实现的
+            // System.out.println("连接OK");
+            connection.close();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("c3p0 5000连接mysql 耗时=" + (end - start));
+    }
+}
