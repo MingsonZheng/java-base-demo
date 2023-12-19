f58d80c0b23932f2428ad96d34448f129b82898b
MingsonZheng <mingsonzheng@outlook.com>
2023-12-19 23:58:59 +0800

    DAO

---
+++ chapter020/src/com/zzm/dao_/dao/ActorDAO/ActorDAO.java
@@ -0,0 +1,13 @@
+package com.zzm.dao_.dao.ActorDAO;
+
+import com.zzm.dao_.dao.BasicDAO;
+import com.zzm.dao_.domain.Actor;
+
+/**
+ * @author Mingson
+ * @version 1.0
+ */
+public class ActorDAO extends BasicDAO<Actor> {
+    // 1. 就有 BasicDAO 的方法
+    // 2. 根据业务需求，可以编写特有的方法。
+}
