a0b60c1559819515182ce1d3659aaed95ab421fc
MingsonZheng <mingsonzheng@outlook.com>
2023-12-19 22:22:08 +0800

    DBUtils源码分析

--- chapter020/src/com/zzm/jdbc/datasource/DBUtils_USE.java
+++ chapter020/src/com/zzm/jdbc/datasource/DBUtils_USE.java
@@ -38,6 +38,43 @@ public class DBUtils_USE {
         //     底层使用反射机制 去获取Actor 类的属性，然后进行封装
         // (6) 1 就是给 sql 语句中的? 赋值，可以有多个值，因为是可变参数Object..。 params
         // (7) 底层得到的ResultSet ，会在query 关闭，关闭PreparedStatement
+        /**
+         * 分析 queryRunner.query 方法：
+         *     public <T> T query(Connection conn, String sql, ResultSetHandler<T> rsh, Object... params) throws SQLException {
+         *         if (conn == null) {
+         *             throw new SQLException("Null connection");
+         *         } else if (sql == null) {
+         *             throw new SQLException("Null SQL statement");
+         *         } else if (rsh == null) {
+         *             throw new SQLException("Null ResultSetHandler");
+         *         } else {
+         *             Statement stmt = null;
+         *             ResultSet resultSet = null;// 接收返回的 ResultSet
+         *             T result = null;// 返回 ArrayList
+         *
+         *             try {
+         *                 if (params != null && params.length > 0) {
+         *                     PreparedStatement ps = this.prepareStatement(conn, sql);// 创建 PreparedStatement
+         *                     stmt = ps;
+         *                     this.fillStatement(ps, params);// 对 sql 进行 ? 赋值
+         *                     resultSet = this.wrap(ps.executeQuery());// 执行 sql，返回 ResultSet
+         *                 } else {
+         *                     stmt = conn.createStatement();
+         *                     resultSet = this.wrap(((Statement)stmt).executeQuery(sql));
+         *                 }
+         *
+         *                 result = rsh.handle(resultSet);// 返回的 ResultSet -> ArrayList [使用到反射，对传入class对象处理]
+         *             } catch (SQLException var12) {
+         *                 this.rethrow(var12, sql, params);
+         *             } finally {
+         *                 this.closeQuietly(resultSet);// 关闭 ResultSet
+         *                 this.closeQuietly((Statement)stmt);// 关闭 PreparedStatement 对象
+         *             }
+         *
+         *             return result;
+         *         }
+         *     }
+         */
         List<Actor> list = queryRunner.query(connection, sql, new BeanListHandler<>(Actor.class), 1);
         System.out.println("输出集合的信息");
         for (Actor actor : list) {
